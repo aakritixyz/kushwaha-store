c9ec70da36fccb0531e03db7a79938b731f139c4
Aakriti Kushwaha <aakriti.kushwaha.ak@gmail.com>
2026-07-14 13:55:52 +0530

    Use native Vercel response methods

--- api/[...path].js
+++ api/[...path].js
@@ -4,8 +4,10 @@ export default async function handler(req, res) {
     return await appHandler(req, res);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    return res.end(JSON.stringify({
       error: error?.message || "Kushwaha Store API failed to start"
-    });
+    }));
   }
 }
